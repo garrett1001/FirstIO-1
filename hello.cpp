6b388db5c10d6e2c5ea4c5ecdc483f3fbb5607e7
garrett1001 <gwelton@mail.csuchico.edu>
2018-01-30 23:58:35 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+//Author: Garrett Welton
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hello, my name is Garrett, what's your name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
